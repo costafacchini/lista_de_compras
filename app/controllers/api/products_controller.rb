befaa8f4252e785f049dfc4d10219ab903dc0044
Alan <costafacchini@gmail.com>
2015-07-07 22:46:31 -0300

    Create all action at ProductsController

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,4 +2,48 @@ class Api::ProductsController < ApplicationController
   def index
     render json: Product.all
   end
+
+  def new
+    render json: Product.new
+  end
+
+  def show
+    render json: Product.find(params[:id])
+  end
+
+  def create
+    product = Product.new(filtered_params)
+
+    if products.save
+      render json: product, status: :created
+    else
+      render json: product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    product = Product.params(params[:id])
+
+    if product.update(filtered_params)
+      render json: product
+    else
+      render json: product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    product = Product.params(params[:id])
+
+    if product.destroy
+      render json: product
+    else
+      render json: product.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def filtered_params
+    params.require(:product).permit(:name)
+  end
 end

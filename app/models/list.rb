487609f85810ab68b8ce95eb3494b9f6da1611d0
Alan <costafacchini@gmail.com>
2015-07-12 16:41:44 -0300

    Adjust associations between Model's

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,4 @@
 class List < ActiveRecord::Base
+  has_many :list_items, dependent: :destroy
+  accepts_nested_attributes_for :list_items, allow_destroy: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ActiveRecord::Base
+  has_many :list_items, dependent: :restrict_with_error
 end
